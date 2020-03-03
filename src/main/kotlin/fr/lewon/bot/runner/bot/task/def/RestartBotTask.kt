7db96b15be9fbde50c999e99c2a4970503e33301
Victor Lewon <viclew@gmail.com>
2020-03-04 00:23:15 +0100

    Save commit

--- src/main/kotlin/fr/lewon/bot/runner/bot/task/def/RestartBotTask.kt
+++ src/main/kotlin/fr/lewon/bot/runner/bot/task/def/RestartBotTask.kt
@@ -3,12 +3,13 @@ package fr.lewon.bot.runner.bot.task.def
 import fr.lewon.bot.runner.Bot
 import fr.lewon.bot.runner.bot.task.BotTask
 import fr.lewon.bot.runner.bot.task.TaskResult
+import fr.lewon.bot.runner.lifecycle.bot.BotLifeCycleOperation
 
 class RestartBotTask(bot: Bot, initialDelayMillis: Long) : BotTask("Restart bot", bot, initialDelayMillis) {
 
     @Throws(Exception::class)
     override fun doExecute(bot: Bot): TaskResult {
-        bot.start()
+        BotLifeCycleOperation.START.run(bot)
         return TaskResult()
     }
 }
